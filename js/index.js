3bf4f27eff0596b1b72a09b490a6d53e34c1d5f6
abubakarjutt6346527@gmail.com <ayeshkay1997@gmail.com>
2023-10-01 13:14:12 +0500

    added validation errors

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ let emailError = document.getElementById("email_error");
 let passwordError = document.getElementById("password_error");
 
 function validated(){
-    let hasError = false;
+    let hasError = false;    // Initialize a variable to track errors
     
     if(emailInput.value.length < 6 ){
         emailInput.style.border = '1px solid red';
         emailError.style.display = 'block';
         emailInput.focus()
-        hasError = true;
+        hasError = true;         // Set hasError to true if there's an error
     } 
     if(passwordInput.value.length < 6 ){
         passwordInput.style.border = '1px solid red';
@@ -22,6 +22,6 @@ function validated(){
         hasError = true;
 
 }
-return !hasError;
+return !hasError;  // Return false only if there's an error, otherwise return true
 }
 
