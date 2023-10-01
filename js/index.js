49520f3bc18e37f68b31d16e624ace8fac9f0891
abubakarjutt6346527@gmail.com <ayeshkay1997@gmail.com>
2023-10-01 13:12:32 +0500

    added validation errors

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// validation code for inputs 
+let emailInput = document.forms['form']['email'];
+// console.log(emailInput)
+let passwordInput = document.forms['form']['password'];
+let emailError = document.getElementById("email_error");
+let passwordError = document.getElementById("password_error");
+
+function validated(){
+    let hasError = false;
+    
+    if(emailInput.value.length < 6 ){
+        emailInput.style.border = '1px solid red';
+        emailError.style.display = 'block';
+        emailInput.focus()
+        hasError = true;
+    } 
+    if(passwordInput.value.length < 6 ){
+        passwordInput.style.border = '1px solid red';
+        passwordError.style.display = 'block';
+        passwordInput.focus()
+        
+        hasError = true;
+
+}
+return !hasError;
+}
+
